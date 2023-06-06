852725bfb8cf62f1489e9ff09e4c89cb6e633d05
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-06-07 11:56:02 +1200

    added EKS - Containerized Stocks App

---
+++ exercises/exercise6/main.tf
@@ -0,0 +1,162 @@
+terraform {
+  required_version = ">= 1.4.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name        = "cloudacademydevops"
+  environment = "demo"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+#====================================
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 48)]
+  intra_subnets   = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 52)]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  manage_default_network_acl    = true
+  default_network_acl_tags      = { Name = "${local.name}-default" }
+  manage_default_route_table    = true
+  default_route_table_tags      = { Name = "${local.name}-default" }
+  manage_default_security_group = true
+  default_security_group_tags   = { Name = "${local.name}-default" }
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+
+  tags = {
+    Name        = "${local.name}-eks"
+    Environment = local.environment
+  }
+}
+
+#====================================
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = ">= 19.15.0"
+
+  cluster_name    = "${local.name}-eks"
+  cluster_version = "1.27"
+
+  cluster_endpoint_public_access = true
+
+  # EKS Addons
+  cluster_addons = {
+    coredns    = {}
+    kube-proxy = {}
+    vpc-cni    = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      use_custom_launch_template = false
+
+      instance_types = ["m5.large"]
+      capacity_type  = "SPOT"
+
+      disk_size = 10
+
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+    }
+  }
+
+  tags = {
+    Name        = "${local.name}-eks"
+    Environment = local.environment
+  }
+}
+
+#====================================
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name = "nginx-ingress"
+
+  repository       = "https://helm.nginx.com/stable"
+  chart            = "nginx-ingress"
+  namespace        = "nginx-ingress"
+  create_namespace = true
+
+  set {
+    name  = "service.type"
+    value = "ClusterIP"
+  }
+
+  set {
+    name  = "controller.service.name"
+    value = "nginx-ingress-controller"
+  }
+}
+
+# resource "helm_release" "argo" {
+#   name = "argo"
+
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   namespace        = "argo"
+#   create_namespace = true
+
+#   # set {
+#   #   name  = "service.type"
+#   #   value = "ClusterIP"
+#   # }
+
+#   # set {
+#   #   name  = "controller.service.name"
+#   #   value = "nginx-ingress-controller"
+#   # }
+# }
