56ca73c8bb60a3ba6ab208dddd626832b3a37bfe
Jeremy Cook <jeremy.cook@cloudacademy.com>
2023-06-22 20:34:13 +1200

    updated notes +  updated outputs

--- exercises/exercise8/outputs.tf
+++ exercises/exercise8/outputs.tf
@@ -1,3 +1,15 @@
+output "domain_fqdn" {
+  value = local.domain_name
+}
+
+output "domain_netbios" {
+  value = local.domain_netbios_name
+}
+
+output "domain_computer_ou" {
+  value = local.domain_computer_ou
+}
+
 output "windows_instance_public_ip" {
   value = aws_instance.server.public_ip
 }
@@ -7,3 +19,10 @@ output "windows_admin_password" {
   value     = rsadecrypt(aws_instance.server.password_data, file("${aws_key_pair.key_pair.key_name}.pem"))
   sensitive = true
 }
+
+#CAUTION - doing this for demo purposes only
+output "domain_admin_password" {
+  value     = random_password.ad_admin_password.result
+  sensitive = true
+}
+
